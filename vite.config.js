919dd780978ee0b34e8ba63e0d8f681cf671f876
YurGoV <yurgov@gmail.com>
2023-06-12 00:24:18 +0300

    init

--- vite.config.js
+++ vite.config.js
@@ -3,15 +3,15 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/vite-proxy-test",
   server: {
     proxy: {
-      "/api": {
-        target: "https://public.api.openprocurement.org", // Replace with your API URL
-        changeOrigin: true,
-        // rewrite: (path) => path.replace(/^\/api/, ''),
+      '/api': {
+        target: 'https://public.api.openprocurement.org', // Replace with your API URL
+        // changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/vite-proxy-test/, ''),
       },
     },
   },
   plugins: [react()],
+  base: '/vite-proxy-test',
 });
